c92bad6e2ef5f3df088f09d71b31591b214d4327
Astronomy-Club <astronomyclub.iitbhu@gmail.com>
2026-02-08 22:00:45 +0530

    added Container

--- src/Animations/ParticleEffect.tsx
+++ src/Animations/ParticleEffect.tsx
@@ -1,7 +1,7 @@
 import { useCallback } from "react";
 import Particles from "react-tsparticles";
 import { loadSlim } from "tsparticles-slim";
-import type { Engine } from "tsparticles-engine";
+import type { Engine, Container } from "tsparticles-engine";
 
 
 const ParticleEffect: React.FC = () => {
